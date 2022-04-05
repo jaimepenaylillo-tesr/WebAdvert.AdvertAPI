eae0719d1dda9ce1d400bb2ec2e45a9a12a1bb89
jose.j.pena <jose.j.pena@bairesdev.com>
2022-04-05 16:34:45 -0400

    2Nd Commit

---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,18 @@
+﻿using AdvertApi.Models;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        public Task<string> Add(AdvertModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
